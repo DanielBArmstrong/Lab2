2a91792631d280028f165cc2053e6d49c5ca3ab8
Dan Armstrong <dabrya@yahoo.com>
2015-01-21 10:14:11 -0700

    added code to Testmethod

--- RepositoryPractice/Lab2/Program.cs
+++ RepositoryPractice/Lab2/Program.cs
@@ -11,12 +11,17 @@ namespace Lab2
         static void Main(string[] args)
         {
           
-
+            
         }
 
         static void Testmethod()
         {
+            int m = 9;
+            for(int i = m; i < 10; i++)
+            {
+                Console.WriteLine(j);
 
+            }
         }
 
         static void TestMethod2()
